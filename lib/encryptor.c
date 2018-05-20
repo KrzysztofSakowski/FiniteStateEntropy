3daa83e918201a54bcd6082cfe83f80ddee4cfb3
Krzysztof Sakowski <krz.sakowski@gmail.com>
2018-05-20 20:10:08 +0200

    Add deterministic random from libsodium

--- lib/encryptor.c
+++ lib/encryptor.c
@@ -1,8 +1,17 @@
-#include <stdlib.h>
 #include "encryptor.h"
 
-static const int SHUFFLE_BLOCK_SIZE = 8;
-const unsigned int SEED = 35256658;
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
+#include <sodium.h>
+
+#include <stdlib.h>
+
+
+static const size_t SHUFFLE_BLOCK_SIZE = 8;
+unsigned char SEED[randombytes_SEEDBYTES] = {
+    1, 2, 3, 4, 5, 6, 7, 8
+};
 
 void swap(ShuffleType* a, ShuffleType* b)
 {
@@ -40,37 +49,44 @@ void rotate2(UnshuffleType* first, UnshuffleType* middle, UnshuffleType* last)
     }
 }
 
-
 void pre_compression_shuffle(ShuffleType* ptr, const size_t SIZE)
 {
-    srand(SEED);
+    const size_t BUFFER_SIZE = SIZE / SHUFFLE_BLOCK_SIZE + sizeof(size_t);
+    unsigned char* buffer = (unsigned char*) malloc(BUFFER_SIZE);
+    randombytes_buf_deterministic(buffer, BUFFER_SIZE, SEED);
 
-    int i, shuffle;
+    size_t i, shuffle;
 
     for (i = 0; i+SHUFFLE_BLOCK_SIZE <= SIZE; i += SHUFFLE_BLOCK_SIZE)
     {
-        shuffle = rand() % SHUFFLE_BLOCK_SIZE;
+        shuffle = buffer[i] % SHUFFLE_BLOCK_SIZE;
         rotate(ptr+i, ptr+i+shuffle, ptr+i+SHUFFLE_BLOCK_SIZE);
     }
 
-    shuffle = rand() % SIZE;
+    shuffle = *((size_t*)(buffer + BUFFER_SIZE - sizeof(size_t))) % SIZE;
 
     rotate(ptr, ptr+shuffle, ptr+SIZE);
+
+    free(buffer);
 }
 
 void pre_decompression_shuffle(UnshuffleType *ptr, const size_t SIZE)
 {
-    srand(SEED);
+    const size_t BUFFER_SIZE = SIZE / SHUFFLE_BLOCK_SIZE + sizeof(size_t);
+    unsigned char* buffer = (unsigned char*) malloc(BUFFER_SIZE);
+    randombytes_buf_deterministic(buffer, BUFFER_SIZE, SEED);
 
-    int i, shuffle;
+    size_t i, shuffle;
 
     for (i = 0; i+SHUFFLE_BLOCK_SIZE <= SIZE; i += SHUFFLE_BLOCK_SIZE)
     {
-        shuffle = rand() % SHUFFLE_BLOCK_SIZE;
+        shuffle = buffer[i] % SHUFFLE_BLOCK_SIZE;
         rotate2(ptr+i, ptr+i+shuffle, ptr+i+SHUFFLE_BLOCK_SIZE);
     }
 
-    shuffle = rand() % SIZE;
+    shuffle = *((size_t*)(buffer + BUFFER_SIZE - sizeof(size_t))) % SIZE;
 
     rotate2(ptr, ptr+shuffle, ptr+SIZE);
+
+    free(buffer);
 }
