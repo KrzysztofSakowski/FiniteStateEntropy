3e5510e7919633a80e5366cd94938e34d150d69c
Krzysztof Sakowski <krz.sakowski@gmail.com>
2018-06-12 19:35:50 +0200

    Add splitting data into blocks

--- tests/encryption_test.cpp
+++ tests/encryption_test.cpp
@@ -31,6 +31,36 @@ protected:
     };
 
     unsigned char seed[32];
+
+    size_t compression_helper(void* dst, size_t dstCapacity, const void* src, size_t srcSize, uint32_t block_id)
+    {
+        EncryptionCtx ctx;
+
+        init_ctx(&ctx, block_id, KEY, 32, IV, 16);
+
+        size_t compression_result = FSE_compress(dst, dstCapacity, src, srcSize, &ctx);
+
+        bool compressionFailed = compression_result == 0 || compression_result == 1 || FSE_isError(compression_result);
+
+        deinit_ctx(&ctx);
+
+        return compression_result;
+    }
+
+    size_t decompression_helper(void* dst, size_t dstCapacity, const void* src, size_t srcSize, uint32_t block_id)
+    {
+        EncryptionCtx ctx;
+
+        init_ctx(&ctx, block_id, KEY, 32, IV, 16);
+
+        size_t decompression_result = FSE_decompress(dst, dstCapacity, src, srcSize, &ctx);
+
+        bool decompressionFailed = decompression_result == 0 || decompression_result == 1 || FSE_isError(decompression_result);
+
+        deinit_ctx(&ctx);
+
+        return decompression_result;
+    }
 };
 
 TEST_F(EncryptorTest, Shuffle)
@@ -95,8 +125,6 @@ TEST_F(EncryptorTest, Integration)
 
     size_t read_bytes = read_file(buffer, BUFFER_SIZE);
 
-    printf("Read %zu\n", read_bytes);
-
     EncryptionCtx ctx;
 
     init_ctx(&ctx, 0, KEY, 32, IV, 16);
@@ -105,37 +133,124 @@ TEST_F(EncryptorTest, Integration)
     BYTE* compressed_buffer = (BYTE*) malloc(BUFFER_SIZE);
     size_t compression_result = FSE_compress(compressed_buffer, BUFFER_SIZE, buffer, read_bytes, &ctx);
 
-    if (compression_result == 0 || compression_result == 1 || FSE_isError(compression_result))
-        printf("Compression error: %zu\n", compression_result);
-    else
-        printf("Compression OK: %zu/%zu, ratio: %f\n", compression_result, read_bytes, (float)(compression_result)/read_bytes);
+    bool compressionFailed = compression_result == 0 || compression_result == 1 || FSE_isError(compression_result);
+
+    ASSERT_FALSE(compressionFailed);
 
     // decompress
     BYTE* decompressed_buffer = (BYTE*) malloc(BUFFER_SIZE);
     size_t decompression_result = FSE_decompress(decompressed_buffer, BUFFER_SIZE, compressed_buffer, compression_result, &ctx);
 
-    if (decompression_result == 0 || decompression_result == 1 || FSE_isError(decompression_result))
-        printf("Decompression error: %zu\n", decompression_result);
-    else
-        printf("Decompression OK: %zu/%zu, ratio: %f\n", decompression_result, read_bytes, (float)(decompression_result)/read_bytes);
+    bool decompressionFailed = decompression_result == 0 || decompression_result == 1 || FSE_isError(decompression_result);
+
+    ASSERT_FALSE(decompressionFailed);
 
     // compare
-
-    int is_ok;
-
-    if (decompression_result == read_bytes)
     {
-        int cmp_result = memcmp(buffer, decompressed_buffer, read_bytes);
-        is_ok = (cmp_result == 0);
-    }
-    else
-        is_ok = 0;
+        int is_ok;
 
-    ASSERT_TRUE(is_ok);
+        if (decompression_result == read_bytes)
+        {
+            int cmp_result = memcmp(buffer, decompressed_buffer, read_bytes);
+            is_ok = (cmp_result == 0);
+        }
+        else
+            is_ok = 0;
+
+        ASSERT_TRUE(is_ok);
+    }
 
     deinit_ctx(&ctx);
 
     free(decompressed_buffer);
     free(compressed_buffer);
     free(buffer);
+}
+TEST_F(EncryptorTest, IntegrationWithBlocks)
+{
+    const uint16_t BLOCK_SIZE = 30000;
+    const size_t BUFFER_SIZE = 100000;
+
+    // get data
+    BYTE* buffer = (BYTE*) malloc(BUFFER_SIZE);
+
+    size_t read_bytes = read_file(buffer, BUFFER_SIZE);
+
+    // calc block count
+    auto block_count = static_cast<uint32_t>(read_bytes / BLOCK_SIZE);
+
+    // compress
+    size_t already_compressed = 0;
+    size_t compressed_size = sizeof(uint32_t) + (block_count-1) * sizeof(uint16_t);
+    BYTE* compressed_buffer = (BYTE*) malloc(BUFFER_SIZE);
+    ((uint32_t*)compressed_buffer)[0] = block_count;
+
+    for (uint32_t block_id = 0; block_id+1 < block_count; ++block_id)
+    {
+        size_t compression_result = compression_helper(compressed_buffer+compressed_size, BUFFER_SIZE - compressed_size,
+                buffer + already_compressed, BLOCK_SIZE, block_id);
+
+        ((uint16_t*)compressed_buffer)[block_id+2] = static_cast<uint16_t>(compression_result);
+
+        compressed_size += compression_result;
+        already_compressed += BLOCK_SIZE;
+    }
+
+    {
+        size_t compression_result = compression_helper(compressed_buffer+compressed_size, BUFFER_SIZE - compressed_size,
+                                                       buffer + already_compressed, BUFFER_SIZE-already_compressed, block_count - 1);
+
+        compressed_size += compression_result;
+    }
+
+    // decompress
+    uint32_t compressed_block_count = ((uint32_t*)compressed_buffer)[0];
+    ASSERT_EQ(compressed_block_count, 3);
+
+    size_t compressed_offset = sizeof(uint32_t) + (compressed_block_count-1) * sizeof(uint16_t);
+    size_t decompressed_offset = 0;
+    BYTE* decompress_buffer = (BYTE*) malloc(BUFFER_SIZE);
+
+    for (uint32_t block_id = 0; block_id+1 < compressed_block_count; ++block_id)
+    {
+        auto current_block_compressed_size = ((uint16_t*)compressed_buffer)[block_id+2];
+
+        size_t decompression_result = decompression_helper(decompress_buffer + decompressed_offset, BUFFER_SIZE - decompressed_offset,
+                                                           compressed_buffer + compressed_offset, current_block_compressed_size, block_id);
+
+        compressed_offset += current_block_compressed_size;
+        decompressed_offset += decompression_result;
+    }
+
+    {
+        size_t decompression_result = decompression_helper(decompress_buffer + decompressed_offset,
+                                                           BUFFER_SIZE - decompressed_offset,
+                                                           compressed_buffer + compressed_offset,
+                                                           compressed_size - compressed_offset, // TODO hacked with: compressed_size
+                                                           compressed_block_count - 1);
+
+        decompressed_offset += decompression_result;
+    }
+
+    // compare
+    {
+        int is_ok;
+
+        ASSERT_EQ(decompressed_offset, read_bytes);
+
+        if (decompressed_offset == read_bytes)
+        {
+            int cmp_result = memcmp(buffer, decompress_buffer, read_bytes);
+            is_ok = (cmp_result == 0);
+        }
+        else
+            is_ok = 0;
+
+        ASSERT_TRUE(is_ok);
+    }
+
+    // clean up
+    free(buffer);
+    free(compressed_buffer);
+    free(decompress_buffer);
 }
